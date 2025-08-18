1a983990cc1e5cefc53261ab4f12337fefac3680
王晨飞 <wangchenfei.work@gmail.com>
2025-08-18 17:34:36 +0800

    fix

--- toolkit/resources/i18n/zh_CN.ts
+++ toolkit/resources/i18n/zh_CN.ts
@@ -25,72 +25,72 @@
 <context>
     <name>SettingView</name>
     <message>
-        <location filename="../../view/setting.py" line="43"/>
+        <location filename="../../view/setting.py" line="44"/>
         <source>Settings</source>
         <translatorcomment>设置</translatorcomment>
         <translation>设置</translation>
     </message>
     <message>
-        <location filename="../../view/setting.py" line="47"/>
+        <location filename="../../view/setting.py" line="49"/>
         <source>Personalization</source>
         <translatorcomment>个性化</translatorcomment>
         <translation>个性化</translation>
     </message>
     <message>
-        <location filename="../../view/setting.py" line="54"/>
+        <location filename="../../view/setting.py" line="56"/>
         <source>Application theme</source>
         <translatorcomment>应用主题</translatorcomment>
         <translation>应用主题</translation>
     </message>
     <message>
-        <location filename="../../view/setting.py" line="55"/>
+        <location filename="../../view/setting.py" line="57"/>
         <source>Change the appearance of your application</source>
         <translatorcomment>调整你的应用的外观</translatorcomment>
         <translation>调整你的应用的外观</translation>
     </message>
     <message>
-        <location filename="../../view/setting.py" line="57"/>
+        <location filename="../../view/setting.py" line="59"/>
         <source>Light</source>
         <translatorcomment>浅色</translatorcomment>
         <translation>浅色</translation>
     </message>
     <message>
-        <location filename="../../view/setting.py" line="58"/>
+        <location filename="../../view/setting.py" line="60"/>
         <source>Dark</source>
         <translatorcomment>深色</translatorcomment>
         <translation>深色</translation>
     </message>
     <message>
-        <location filename="../../view/setting.py" line="67"/>
+        <location filename="../../view/setting.py" line="69"/>
         <source>Theme color</source>
         <translatorcomment>主题色</translatorcomment>
         <translation>主题色</translation>
     </message>
     <message>
-        <location filename="../../view/setting.py" line="68"/>
+        <location filename="../../view/setting.py" line="70"/>
         <source>Change the theme color of you application</source>
         <translatorcomment>调整你的应用的主题色</translatorcomment>
         <translation>调整你的应用的主题色</translation>
     </message>
     <message>
-        <location filename="../../view/setting.py" line="76"/>
+        <location filename="../../view/setting.py" line="78"/>
         <source>Language</source>
         <translatorcomment>语言</translatorcomment>
         <translation>语言</translation>
     </message>
     <message>
-        <location filename="../../view/setting.py" line="77"/>
+        <location filename="../../view/setting.py" line="79"/>
         <source>Set your preferred language for UI</source>
         <translatorcomment>选择界面所使用的语言</translatorcomment>
         <translation>选择界面所使用的语言</translation>
     </message>
     <message>
-        <location filename="../../view/setting.py" line="108"/>
+        <location filename="../../view/setting.py" line="110"/>
         <source>Updated successfully</source>
         <translation>更新成功</translation>
     </message>
     <message>
-        <location filename="../../view/setting.py" line="109"/>
+        <location filename="../../view/setting.py" line="111"/>
         <source>Configuration takes effect after restart</source>
         <translatorcomment>配置在重启软件后生效</translatorcomment>
         <translation>配置在重启软件后生效</translation>
