f204a7da1ff9378c866f2748209b27b26d02b462
王晨飞 <wangchenfei.work@gmail.com>
2025-08-18 16:58:36 +0800

    i18n

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Toolkit
 ```powershell
 pyside6-rcc.exe .\toolkit\resources\resources.qrc -o .\toolkit\utils\resources.py
 ```
+
+### 国际化
+```powershell
+pyside6-lupdate.exe .\toolkit\core.py .\toolkit\view\setting.py -ts .\toolkit\resources\i18n\zh_CN.ts
+pyside6-lrelease.exe .\toolkit\resources\i18n\zh_CN.ts -qm .\toolkit\resources\i18n\zh_CN.qm
+```
---
+++ toolkit/resources/i18n/zh_CN.ts
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../core.py" line="32"/>
+        <source>Home</source>
+        <translatorcomment>主页</translatorcomment>
+        <translation>主页</translation>
+    </message>
+    <message>
+        <location filename="../../core.py" line="41"/>
+        <source>PDF</source>
+        <translatorcomment>PDF</translatorcomment>
+        <translation>PDF</translation>
+    </message>
+    <message>
+        <location filename="../../core.py" line="50"/>
+        <source>Setting</source>
+        <translatorcomment>设置</translatorcomment>
+        <translation>设置</translation>
+    </message>
+</context>
+<context>
+    <name>SettingView</name>
+    <message>
+        <location filename="../../view/setting.py" line="43"/>
+        <source>Setting</source>
+        <translatorcomment>设置</translatorcomment>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../../view/setting.py" line="47"/>
+        <source>Personalization</source>
+        <translatorcomment>个性化</translatorcomment>
+        <translation>个性化</translation>
+    </message>
+    <message>
+        <location filename="../../view/setting.py" line="54"/>
+        <source>Application theme</source>
+        <translatorcomment>应用主题</translatorcomment>
+        <translation>应用主题</translation>
+    </message>
+    <message>
+        <location filename="../../view/setting.py" line="55"/>
+        <source>Change the appearance of your application</source>
+        <translatorcomment>调整你的应用的外观</translatorcomment>
+        <translation>调整你的应用的外观</translation>
+    </message>
+    <message>
+        <location filename="../../view/setting.py" line="57"/>
+        <source>Light</source>
+        <translatorcomment>浅色</translatorcomment>
+        <translation>浅色</translation>
+    </message>
+    <message>
+        <location filename="../../view/setting.py" line="58"/>
+        <source>Dark</source>
+        <translatorcomment>深色</translatorcomment>
+        <translation>深色</translation>
+    </message>
+    <message>
+        <location filename="../../view/setting.py" line="67"/>
+        <source>Theme color</source>
+        <translatorcomment>主题色</translatorcomment>
+        <translation>主题色</translation>
+    </message>
+    <message>
+        <location filename="../../view/setting.py" line="68"/>
+        <source>Change the theme color of you application</source>
+        <translatorcomment>调整你的应用的主题色</translatorcomment>
+        <translation>调整你的应用的主题色</translation>
+    </message>
+    <message>
+        <location filename="../../view/setting.py" line="76"/>
+        <source>Language</source>
+        <translatorcomment>语言</translatorcomment>
+        <translation>语言</translation>
+    </message>
+    <message>
+        <location filename="../../view/setting.py" line="77"/>
+        <source>Set your preferred language for UI</source>
+        <translatorcomment>选择界面所使用的语言</translatorcomment>
+        <translation>选择界面所使用的语言</translation>
+    </message>
+    <message>
+        <location filename="../../view/setting.py" line="108"/>
+        <source>Updated successfully</source>
+        <translation>更新成功</translation>
+    </message>
+    <message>
+        <location filename="../../view/setting.py" line="109"/>
+        <source>Configuration takes effect after restart</source>
+        <translatorcomment>配置在重启软件后生效</translatorcomment>
+        <translation>配置在重启软件后生效</translation>
+    </message>
+</context>
+</TS>
